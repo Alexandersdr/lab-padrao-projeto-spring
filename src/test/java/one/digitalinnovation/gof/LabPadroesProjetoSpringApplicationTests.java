76374f6d6ce0206ee8f441d07a60d427a6dec855
Alexander Dev <82269375+Alexandersdr@users.noreply.github.com>
2022-11-27 14:37:07 -0300

    new

--- src/test/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplicationTests.java
+++ src/test/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LabPadraoProjetoSpringApplicationTests {
+class LabPadroesProjetoSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
